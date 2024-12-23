315af82be81c0e8c9490a1b94d3369ef84147afb
Paras Munoli <parasmunoli@gmail.com>
2024-12-23 16:58:31 +0530

    Cleaned the Scraped data and split it into batches

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import streamlit as st
-from scrape import scrapingWebsite
+from scrape import (
+    scrapingWebsite,
+    extractBody,
+    cleanText,
+    splitText
+)
 
 st.title("AI Web Scraper")
 url = st.text_input("Enter Website URL:")
@@ -7,4 +12,18 @@ url = st.text_input("Enter Website URL:")
 if st.button("Scrape"):
     st.write("Scraping...")
     result = scrapingWebsite(url)
-    print(result)
+    content = extractBody(result)
+    cleanContent = cleanText(content)
+
+    st.session_state.text = cleanContent
+
+    with st.expander("See Scraped Text"):
+        st.text_area("Scraped Text", cleanContent, height=400)
+
+if "text" in st.session_state:
+    parseDescription = st.text_input("Enter Parsing Description:")
+
+    if st.button("Parse"):
+        if parseDescription:
+            st.write("Parsing...")
+            chunks = splitText(st.session_state.text)
